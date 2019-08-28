77627f3f7d7dff1397a2177603feccce692200d1
BreLightYear <brenomgmartins@gmail.com>
2019-08-28 20:25:56 -0300

    Alterado banco de dados de cadastro

---
+++ ProjectCloud/account/choices.py
@@ -0,0 +1,18 @@
+
+
+TYPE_SEX = [
+    ('0'),
+    ('1', "Homem"),
+    ('2', "Mulher")
+]
+
+REGION = [
+    ('0'),
+    ('1', "Sao Paulo"),
+    ('2', "Rio de Janeiro")
+]
+
+COUNTRY = [
+    ('0'),
+    ('1', "Brasil")
+]
--- ProjectCloud/account/models.py
+++ ProjectCloud/account/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from choices import TYPE_SEX, REGION, COUNTRY
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class Person(models.Model):
+    name= models.CharField(max_length=50, verbose_name=_('Nome'), help_text=('Digite seu nome'))
+    name2= models.CharField(max_length=50, verbose_name=_('Sobrenome'), help_text=('Digite seu sobrenome'))
+    email= models.EmailField(max_length=254, verbose_name=_('E-mail'), help_text=('Digite seu e-mail'))
+    country= models.CharField(max_length=50, choices=COUNTRY)
+    region= models.CharField(max_length=50, choices=REGION)
+    adress= models.CharField(max_length=50, verbose_name=_('Endereço'), help_text=('Digite seu endereço'))
+    sex= models.CharField(choices=blank= False, max_length=50, verbose_name=_('Sexo'), help_text=('Selecione seu sexo'))
+    created_in= models.DateTimeField(auto_now=False)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Empresa(models.Model):
+    company= models.CharField(max_length=50, verbose_name=_('Empresa'), help_text=('Digite o nome'))
+    country= models.CharField(max_length=50, choices=COUNTRY)
+    region= models.CharField(max_length=50, choices=REGION)
+    adress= models.CharField(max_length=50, verbose_name=_('Endereço'), help_text=('Digite seu endereço'))
+
+
+    def __str__(self):
+        return self.company
