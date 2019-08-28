85689480ac01aca0ddea6d6266152b38304ecb72
BreLightYear <brenomgmartins@gmail.com>
2019-08-28 20:43:41 -0300

    Adicionado aplicativo Empresa nas definições

--- ProjectCloud/account/models.py
+++ ProjectCloud/account/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from choices import TYPE_SEX, REGION, COUNTRY
 from django.utils.translation import gettext_lazy as _
+from geography.models import ZipCode
 
 # Create your models here.
 
@@ -10,6 +11,7 @@ class Person(models.Model):
     email= models.EmailField(max_length=254, verbose_name=_('E-mail'), help_text=('Digite seu e-mail'))
     country= models.CharField(max_length=50, choices=COUNTRY)
     region= models.CharField(max_length=50, choices=REGION)
+    zip_code = models.ForeignKey(ZipCode, on_delete=models.SET_NULL, blank=True, null=True,)
     adress= models.CharField(max_length=50, verbose_name=_('Endereço'), help_text=('Digite seu endereço'))
     sex= models.CharField(choices=blank= False, max_length=50, verbose_name=_('Sexo'), help_text=('Selecione seu sexo'))
     created_in= models.DateTimeField(auto_now=False)
