8f7a171087abc2a9081f5d0e16d9811bdeb12a17
BreLightYear <brenomgmartins@gmail.com>
2019-08-28 21:34:37 -0300

    Commit com alterações estruturais

--- ProjectCloud/ProjectCloud/urls.py
+++ ProjectCloud/ProjectCloud/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, url
+from templates import views
+from views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$',views.singIn),
+    url(r'^postsign/',views.postsign),
 ]
---
+++ ProjectCloud/templates/views.py
@@ -0,0 +1,20 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.4.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#config-web-app -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAUD8Unx3eBggKXkMENuWW1uOjrZsuuQxI",
+    authDomain: "projectcloud-6b351.firebaseapp.com",
+    databaseURL: "https://projectcloud-6b351.firebaseio.com",
+    projectId: "projectcloud-6b351",
+    storageBucket: "",
+    messagingSenderId: "945657686442",
+    appId: "1:945657686442:web:408f07d5baa91cfc"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
