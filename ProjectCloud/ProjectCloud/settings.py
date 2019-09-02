d0c2f07e8c63b4072d6cb478d96a09e4dbf148a7
BreLightYear <brenomgmartins@gmail.com>
2019-09-01 22:46:39 -0300

    Adicionado DjangoRestFramework

--- ProjectCloud/ProjectCloud/settings.py
+++ ProjectCloud/ProjectCloud/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_^+72kg$0n6l%##l7ksj9wyi12o8h28+0weu4tx%4_k*1@o71e'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'api',
     'account',
     'empresa',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK  = {
+     # Use as permissões padrão `django.contrib.auth` do Django, 
+    # ou permita acesso somente leitura para usuários não autenticados. 
+    ' DEFAULT_PERMISSION_CLASSES ' : [
+         ' rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly '
+    ]
+}
