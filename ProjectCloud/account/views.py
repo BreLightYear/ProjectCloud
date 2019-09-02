6579e7bc8a649bdce30b03fcea8c406315bbecd0
BreLightYear <brenomgmartins@gmail.com>
2019-09-02 19:45:17 -0300

    Adicionado view restframework

--- ProjectCloud/account/views.py
+++ ProjectCloud/account/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Person
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
+
+class PersonList(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'profile_detail.html'
+
+    def get(self, request, pk):
+        person = get_object_or_404(Person, pk=pk)
+        serializer = PersonSerializer(person)
+        return Response({'serializer': serializer, 'person': person})
+
+    def post(self, request, pk):
+        person = get_object_or_404(Person, pk=pk)
+        serializer = PersonSerializer(person, data=request.data)
+        if not serializer.is_valid():
+            return Response({'serializer': serializer, 'person': person})
+        serializer.save()
+        return redirect('profile-list')
