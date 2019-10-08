76bb900276dd60e6eb2cc0e248d2a9f6a1bd3823
BreLightYear <brenomgmartins@gmail.com>
2019-10-08 11:03:22 -0300

    Adicionado campo para o usuáro ser profossional

--- ProjectCloud/account/models.py
+++ ProjectCloud/account/models.py
@@ -9,7 +9,8 @@ from .validators import validate_CPF, validate_CNPJ
 # Create your models here.
 
 class Person(models.Model):
-    #id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    picture = models.FileField(upload_to=None, max_length=100)
     active = models.BooleanField( default= False, max_length=20, verbose_name=_('Ativo'))
     name = models.CharField(max_length=50, verbose_name=_('Nome'), help_text=('Nome do Usuário'))
     name2 = models.CharField(max_length=50, verbose_name=_('Sobrenome'), help_text=('Sobrenome do Usuário'))
@@ -19,14 +20,18 @@ class Person(models.Model):
     region = models.CharField(max_length=50, choices=REGION, default=0, verbose_name=_('Região'))
     adress = models.CharField(max_length=50, verbose_name=_('Endereço'), help_text=('Endereço do Usuário'))
     sex = models.CharField(choices= TYPE_SEX, blank= False, max_length=50, verbose_name=_('Sexo'))
+    is_professional = models.BooleanField(max_length=12, default=False, verbose_name=_('Ativo'), help_text=_('Se o usuário for Empresa/Profissional Ativado'))
     #created_in= models.DateTimeField(auto_now_add=False, default=timezone.now)
 
 
     def __str__(self):
         return self.name
 
+    def __str__(self):
+        return self.id
+
 class Company(models.Model):
-    #id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     active = models.BooleanField(default=False, max_length=50, verbose_name=_('Ativo'))
     name = models.CharField(max_length=50, blank=True, help_text=('Nome da organização'))
     cnpj = models.CharField(unique=True, validators=[validate_CNPJ], verbose_name=_('CNPJ'), max_length=50)
@@ -39,3 +44,7 @@ class Company(models.Model):
 
     def __str__(self):
         return self.name
+
+class Dots(models.Model):
+    person = models.ForeignKey(Person, verbose_name=_("Person"), on_delete=models.CASCADE)
+    dots = models.BigIntegerField(editable=False, verbose_name=_('Dots'))
---
+++ ProjectCloud/account/utils.py
@@ -0,0 +1,10 @@
+from .models import Person, Company
+
+
+def is_professional(is_professional=None):
+    if is_professional in Person is True:
+        return True
+    else:
+        return False
+
+def Dots()
